487652ce4b7f6b55a1706c4882bab77782467fd6
LoneWolf229 <smarthari2299@gmail.com>
2022-10-22 21:12:38 +0530

    changes

--- client/src/Pages/Canvas.js
+++ client/src/Pages/Canvas.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import CanvasDraw from "react-canvas-draw";
 
-function App() {
+
+const Canvas = () => {
+
+
+
+
     return(
         <CanvasDraw 
             canvasHeight={750}
             canvasWidth={1500}
             brushRadius = {1}
             brushColor = {"#" + Math.floor(Math.random() * 16777215).toString(16)}
+            hideGrid={true}
          />
     )
         
 }
 
-export default App
+export default Canvas
